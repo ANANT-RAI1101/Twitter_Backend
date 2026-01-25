ce3b0ab394b6bdba6a3580807f448f45850ad135
ANANT-RAI1101 <anantrai1101@gmail.com>
2026-01-25 19:36:17 +0530

    configured the tweet model and added like service

--- src/repository/tweet-repository.js
+++ src/repository/tweet-repository.js
@@ -1,9 +1,34 @@
 import Tweet from "../models/tweet.js";
 import CrudRepository from "./crud-repository.js";
+import { AppError, ValidationError } from "../utils/Errors/index.js";
+import { StatusCodes } from "http-status-codes";
 
-class TweetRepository extends CrudRepository{
-    constructor(){
+class TweetRepository extends CrudRepository {
+    constructor() {
         super(Tweet);
+    }
+
+    async find(id) {
+        try {
+            const tweet = await Tweet.findById(id).populate({ path: 'likes' });
+            if (!tweet) {
+                throw new AppError(
+                    "Not Found",
+                    "Resource not found",
+                    "The requested record does not exist",
+                    StatusCodes.NOT_FOUND
+                )
+            }
+            return tweet;
+        } catch (error) {
+            if (error instanceof AppError) throw error;
+            throw new AppError(
+                "Repository Error",
+                "cannot get what requested ",
+                "there is some error in fetching the request . Please try again later",
+                StatusCodes.INTERNAL_SERVER_ERROR
+            )
+        }
     }
 }
 
---
+++ src/services/liker-service.js
@@ -0,0 +1,51 @@
+import { TweetRepository, LikeRepository } from "../repository/index.js";
+import { AppError, ServiceError } from "../utils/Errors/index.js";
+import { StatusCodes } from "http-status-codes";
+
+class LikeService {
+    constructor() {
+        this.likeRepository = new LikeRepository(),
+            this.tweetRepository = new TweetRepository()
+    }
+
+    async toggleLike(modelId, modelType, userId) {
+        try {
+            if (modelType = "Tweet") {
+                var likeable = await this.tweetRepository.find(modelId)
+            } else if (modelType = "Comments") {
+
+            } else {
+                throw new AppError(
+                    "Service Layer Error",
+                    'unknown model type',
+                    'unknown model type',
+                    StatusCodes.BAD_REQUEST
+                );
+            }
+            const exists = await this.likeRepository.findByUserAndLikeable({
+                user: userId,
+                onModel: modelType,
+                likeable: modelId
+            })
+            if (exists) {
+                likeable.likes.pull(exists.id)
+                await likeable.save()
+                await exists.remove()
+                var isAdded = false;
+            } else {
+                const newLike = await this.likeRepository.create({
+                    user: userId,
+                    onModel: modelType,
+                    likeable: modelId
+                });
+                likeable.likes.push(newLike)
+                var isAdded = True;
+            }
+            return isAdded
+        } catch (error) {
+            throw new ServiceError()
+        }
+    }
+}
+
+export default LikeService
