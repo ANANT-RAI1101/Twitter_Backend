c2b538c68157cd36f48b0a6cfa920879f9627cca
ANANT-RAI1101 <anantrai1101@gmail.com>
2026-01-26 19:24:21 +0530

    added comment model and repo

---
+++ src/models/comment.js
@@ -0,0 +1,28 @@
+import mongoose from "mongoose";
+
+const commentSchema=new mongoose.Schema({
+    content:{
+        type:String,
+        required:true
+    },
+    onModel:{
+        type:mongoose.Schema.Types.ObjectId,
+        required:true,
+        enum:["Tweet","Comment"]
+    },
+    commentable:{
+        type:mongoose.Schema.Types.ObjectId,
+        required:true,
+        refPath:onModel
+    },
+    comments:[
+        {
+            type:mongoose.Schema.Types.ObjectId,
+            ref:"Comment"
+        }
+    ]
+},{timestamps:true})
+
+const Comment=mongoose.model("Comment",commentSchema)
+
+export default Comment
