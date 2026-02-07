922f3f440bcf16c62d612337c70c25db44e2c5cd
ANANT-RAI1101 <anantrai1101@gmail.com>
2026-02-07 08:25:46 +0530

    added comment controller and routes

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import { createTweet,getTweet } from '../../controllers/tweet-controller.js';
 import { signup,login } from '../../controllers/user-controller.js';
+import { createComment } from '../../controllers/comment-controller.js';
+import { authenticate } from '../../middlewares/auth-middleware.js';
 const router=express.Router();
 
 router.post("/tweets",createTweet)
@@ -8,5 +10,9 @@ router.get("/tweets/:id",getTweet)
 
 router.post("/signup",signup)
 router.post("/login",login)
+
+router.post("/comments",authenticate,createComment)
+
+
 
 export default router
--- src/services/comment-service.js
+++ src/services/comment-service.js
@@ -38,3 +38,5 @@ class CommentService {
         }
     }
 }
+
+export default CommentService
